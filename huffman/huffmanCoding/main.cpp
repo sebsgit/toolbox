bfd712a4922c5d4ac0c46910b6776e13f81a7719
sebsgit <s.baginski@mail.com>
2016-12-17 17:47:31 +0100

    creating huffman codes for given dictionary

--- huffman/huffmanCoding/main.cpp
+++ huffman/huffmanCoding/main.cpp
@@ -4,7 +4,9 @@
 #include <algorithm>
 #include <cassert>
 #include <type_traits>
+#include <tuple>
 #include <unordered_map>
+#include <sstream>
 
 namespace huffman{
 
@@ -64,6 +66,9 @@ protected:
 			dfs_iterator_base result(_previous, _current);
 			this->operator++();
 			return result;
+		}
+		bool is_leaf() const {
+			return _current ? _current->is_leaf() : false;
 		}
 	protected:
 		NodeType _previous;
@@ -140,7 +145,9 @@ public:
 	iterator end() const {
 		return iterator(nullptr);
 	}
-
+	bool is_leaf() const {
+		return !_left && !_right;
+	}
 protected:
 	binary_tree() {
 	}
@@ -184,32 +191,122 @@ static void test_tree_iterator() {
 }
 
 namespace huffman {
-	template <typename T, typename Real=float>
-	class probability_table {
-		static_assert(std::is_floating_point<Real>::value, "probability_table entry needs to have a floating point probability.");
-	public:
-		virtual ~probability_table() {}
-		virtual Real operator[] (const T& t) const = 0;
-	};
 	template <typename T, typename Real = float>
-	class probability_map : public probability_table<T, Real> {
+	class probability_table {
+		static_assert(std::is_arithmetic<Real>::value, "probability_table entry needs to have an arithmetic probability.");
 	public:
-		Real operator[] (const T& t) const override {
+		using value_type = T;
+		using probability_type = Real;
+		using const_iterator = typename std::unordered_map<T, Real>::const_iterator;
+		using iterator = typename std::unordered_map<T, Real>::iterator;
+
+		Real operator[] (const T& t) const {
 			return _data.at(t);
 		}
 		Real& operator[] (const T& t) {
 			return _data[t];
 		}
+		const_iterator cbegin() const {
+			return _data.cbegin();
+		}
+		const_iterator cend() const {
+			return _data.cend();
+		}
+		iterator begin() {
+			return _data.begin();
+		}
+		iterator end() {
+			return _data.end();
+		}
+		size_t empty() const {
+			return _data.empty();
+		}
 	private:
 		std::unordered_map<T, Real> _data;
+	};
+	class code {
+	public:
+		code& prepend(uint8_t bit) {
+			_data.insert(_data.begin(), bit);
+		}
+		std::string to_string() const {
+			std::stringstream result;
+			for (auto i : _data)
+				result << std::to_string(i);
+			return result.str();
+		}
+	private:
+		std::vector<uint8_t> _data;
+	};
+	template <typename T, typename Real=float>
+	class encoder {
+		using probability_map = probability_table<T, Real>;
+	public:
+		void set_probability_table(const probability_map& table) {
+			_probabilities = table;
+			if (!_probabilities.empty())
+				this->recalculate();
+		}
+		code get_code(const T& value) const {
+			return this->_codebook.at(value);
+		}
+	protected:
+		void recalculate() {
+			assert(_probabilities.empty() == false);
+			using node_type = std::tuple<T, Real, code>;
+			using tree = typename binary_tree<node_type>::ptr;
+			auto comparator = [](const tree& left, const tree& right) {
+				return std::get<1>(left->value()) < std::get<1>(right->value());
+			};
+			auto erase_min = [comparator](std::vector<tree>& d) {
+				auto min0 = std::min_element(d.begin(), d.end(), comparator);
+				auto result = *min0;
+				d.erase(min0);
+				return result;
+			};
+			auto prepend_leafs = [](tree t, uint8_t n) {
+				for (auto it = t->begin() ; it != t->end() ; ++it)
+					if (it.is_leaf()) {
+						auto &code = std::get<2>(*it);
+						code.prepend(n);
+					}
+			};
+			std::vector<tree> data;
+			for (auto it : _probabilities)
+				data.push_back(binary_tree<node_type>::make_node(std::make_tuple(it.first, it.second, code())));
+			while (data.size() > 1) {
+				auto min0_value = erase_min(data);
+				auto min1_value = erase_min(data);
+				auto new_probability = std::get<1>(min0_value->value()) + std::get<1>(min1_value->value());
+				node_type new_node = std::make_tuple(T(), Real(new_probability), code());
+				auto root = binary_tree<node_type>::make_node(std::move(new_node));
+				prepend_leafs(min0_value, 0);
+				prepend_leafs(min1_value, 1);
+				root->set_left(min0_value);
+				root->set_right(min1_value);
+				data.push_back(root);
+			}
+			this->_codebook.clear();
+			for (auto it = data[0]->cbegin() ; it != data[0]->cend() ; ++it)
+				if (it.is_leaf())
+					this->_codebook.insert({std::get<0>(*it), std::get<2>(*it)});
+		}
+	private:
+		probability_map _probabilities;
+		std::unordered_map<T, code> _codebook;
 	};
 }
 
 int main(int argc, char *argv[]) {
 	test_tree_iterator();
-	huffman::probability_map<int, float> probs;
+	huffman::probability_table<int, float> probs;
 	probs[1] = 0.14;
 	probs[2] = 0.44;
-	probs[3] = 0.42;
+	probs[7] = 0.23;
+	probs[32] = 0.19;
+	huffman::encoder<int, float> encoder;
+	encoder.set_probability_table(probs);
+	for (auto it : probs)
+		std::cout << it.first << ' ' << encoder.get_code(it.first).to_string() << '\n';
 	return 0;
 }
