4b1593e1e07c69b48134e3cc19bf882d5f42b1fb
Sebastian Baginski <s.baginski@mail.com>
2023-11-13 20:02:59 +0100

    'fixed' output folder permission fail on linux

--- cpp_enum_utils_generator/scripts/call_generate_enum_utils.sh
+++ cpp_enum_utils_generator/scripts/call_generate_enum_utils.sh
@@ -9,9 +9,9 @@ do
 	cat $path > $TMP_CXX
 	
 	TMP_XML=$(mktemp --suffix .xml)
-	~/castxml/bin/castxml --castxml-gccxml --castxml-cc-gnu g++ $TMP_CXX -o $TMP_XML
+	/opt/castxml/bin/castxml --castxml-gccxml --castxml-cc-gnu g++ $TMP_CXX -o $TMP_XML
 	
-	OUTPUT_FILE=/tmp/output/autogen_$(basename $path)
-	python3 ~/scripts/generate_enum_utils.py $TMP_XML > $OUTPUT_FILE
+	OUTPUT_FILE=/var/opt/castxml/output/autogen_$(basename $path)
+	python3 /var/opt/castxml/scripts/generate_enum_utils.py $TMP_XML > $OUTPUT_FILE
 	echo "Code generated in: $(basename $OUTPUT_FILE)"
 done
