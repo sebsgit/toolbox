5abad074f3490d547689b1e49efd1aa0e5e3c2b8
Sebastian Baginski <s.baginski@mail.com>
2024-04-17 22:33:29 +0200

    I2C interrupt implementation

--- STM32F411x_drivers/include/STM32F411x_I2C.h
+++ STM32F411x_drivers/include/STM32F411x_I2C.h
@@ -5,6 +5,27 @@
 
 #include <stdint.h>
 
+// I2C register bit positions
+#define ST_I2C_SR1_SB			(0)
+#define ST_I2C_SR1_ADDR			(1)
+#define ST_I2C_SR1_BTF			(2)
+#define ST_I2C_SR1_ADDR10		(3)
+#define ST_I2C_SR1_STOPF		(4)
+#define ST_I2C_SR1_RXNE			(6)
+#define ST_I2C_SR1_TXE			(7)
+#define ST_I2C_SR1_BERR			(8)		// bus error
+#define ST_I2C_SR1_ARLO			(9)		// arbitration loss
+#define ST_I2C_SR1_AF			(10)	// ACK failure
+#define ST_I2C_SR1_OVR			(11)	// over- or underrun
+#define ST_I2C_SR1_PEC			(12)	// PEC error
+#define ST_I2C_SR1_TMOUT		(14)	// timeout error
+
+#define ST_I2C_SR2_MSL			(0)
+
+#define ST_I2C_CR2_ITERREN		(8)
+#define ST_I2C_CR2_ITEVEN		(9)
+#define ST_I2C_CR2_ITBUFEN		(10)
+
 #define ST_I2C_ACK_DISABLE 		0
 #define ST_I2C_ACK_ENABLE  		1
 
@@ -21,6 +42,16 @@
 #define ST_I2C_IRQ_STATE_IDLE 		(0)
 #define ST_I2C_IRQ_STATE_BUSY_RX	(1)
 #define ST_I2C_IRQ_STATE_BUSY_TX	(2)
+
+#define ST_I2C_EVENT_RX_COMPL		(1)
+#define ST_I2C_EVENT_TX_COMPL		(2)
+#define ST_I2C_EVENT_STOP			(3)
+#define ST_I2C_EVENT_ERR_BUS		(4)
+#define ST_I2C_EVENT_ERR_ARBITRATION_LOSS (5)
+#define ST_I2C_EVENT_ERR_ACK_FAIL	(6)
+#define ST_I2C_EVENT_ERR_OVERRUN	(7)
+#define ST_I2C_EVENT_ERR_PEC		(8)
+#define ST_I2C_EVENT_ERR_TIMEOUT	(9)
 
 typedef struct
 {
@@ -52,6 +83,8 @@ typedef struct
 extern void ST_I2C_init(ST_I2C_t *i2c);
 extern void ST_I2C_deinit(ST_I2C_t *i2c);
 
+extern uint8_t ST_I2C_is_master(ST_I2C_t *i2c);
+
 // blocking API
 extern void ST_I2C_Master_send(ST_I2C_t *i2c, const uint8_t slave_addr, const uint8_t *tx_buffer, const size_t data_len);
 
@@ -60,11 +93,15 @@ extern void ST_I2C_Master_receive(ST_I2C_t *i2c, const uint8_t slave_addr, uint8
 
 extern void ST_I2C_irq_control(ST_I2C_t *i2c, uint8_t priority, uint8_t enable);
 
+extern void ST_I2C_irq_ev_handler(ST_I2C_t *i2c);
+extern void ST_I2C_irq_err_handler(ST_I2C_t *i2c);
+
 // interrupt-based API
 extern uint8_t ST_I2C_Master_send_IT(ST_I2C_t *i2c, const uint8_t slave_addr, const uint8_t *tx_buffer, const size_t data_len);
 
 // interrupt-based API
 extern uint8_t ST_I2C_Master_receive_IT(ST_I2C_t *i2c, const uint8_t slave_addr, uint8_t *rx_buffer, const size_t data_len);
 
+extern void ST_I2C_App_Event(ST_I2C_t *pSpi, uint8_t e_type) __attribute__((weak));
 
 #endif // STM32F411X_I2C_H_
--- STM32F411x_drivers/source/STM32F411x_I2C.c
+++ STM32F411x_drivers/source/STM32F411x_I2C.c
@@ -1,6 +1,8 @@
 #include "STM32F411x_I2C.h"
 #include "STM32F411x_RCC.h"
 
+#include <string.h>
+
 void ST_I2C_init(ST_I2C_t *i2c)
 {
 	i2c->baseAdress->CR1 &= ~(1 << 0); // peripheral disable
@@ -79,6 +81,11 @@ void ST_I2C_deinit(ST_I2C_t *i2c)
 	i2c->baseAdress->CR1 |= (1 << 15); // peripheral software reset
 }
 
+uint8_t ST_I2C_is_master(ST_I2C_t *i2c)
+{
+	return i2c->baseAdress->SR2 & (1 << ST_I2C_SR2_MSL);
+}
+
 void ST_I2C_Master_send(ST_I2C_t *i2c, const uint8_t slave_addr, const uint8_t *tx_buffer, const size_t data_len)
 {
 	// clear ACK failure
@@ -116,15 +123,15 @@ void ST_I2C_Master_send(ST_I2C_t *i2c, const uint8_t slave_addr, const uint8_t *
 	for (size_t i = 0; i < data_len; ++i)
 	{
 		// wait for TxE
-		while (!(i2c->baseAdress->SR1 & (1 << 7)));
+		while (!(i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_TXE)));
 
 		// write DR
 		i2c->baseAdress->DR = tx_buffer[i];
 	}
 
 	// data transmission finalization: wait for TxE=1 and BTF=1
-	while (!(i2c->baseAdress->SR1 & (1 << 7)));
-	while (!(i2c->baseAdress->SR1 & (1 << 2)));
+	while (!(i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_BTF)));
+	while (!(i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_TXE)));
 
 	// generate stop condition
 	i2c->baseAdress->CR1 |= (1 << 9);
@@ -136,9 +143,48 @@ void ST_I2C_Master_receive(ST_I2C_t *i2c, const uint8_t slave_addr, uint8_t *rx_
 	// Reference manual "STM32F411xC/E advanced Arm-based 32-bit MCUs", page 482
 }
 
+static uint32_t ST_I2C_get_ev_irq_no(ST_I2C_t *i2c)
+{
+	switch ((uint32_t)i2c->baseAdress)
+	{
+	case ST_I2C1_BASE_ADDRESS:
+		return ST_NVIC_I2C1_EV;
+	case ST_I2C2_BASE_ADDRESS:
+		return ST_NVIC_I2C2_EV;
+	case ST_I2C3_BASE_ADDRESS:
+		return ST_NVIC_I2C3_EV;
+	default:
+		break;
+	}
+	return 0;
+}
+
+static uint32_t ST_I2C_get_err_irq_no(ST_I2C_t *i2c)
+{
+	switch ((uint32_t)i2c->baseAdress)
+	{
+	case ST_I2C1_BASE_ADDRESS:
+		return ST_NVIC_I2C1_ER;
+	case ST_I2C2_BASE_ADDRESS:
+		return ST_NVIC_I2C2_ER;
+	case ST_I2C3_BASE_ADDRESS:
+		return ST_NVIC_I2C3_ER;
+	default:
+		break;
+	}
+	return 0;
+}
+
 void ST_I2C_irq_control(ST_I2C_t *i2c, uint8_t priority, uint8_t enable)
 {
-
+	const uint32_t irq_err = ST_I2C_get_err_irq_no(i2c);
+	const uint32_t irq_ev = ST_I2C_get_ev_irq_no(i2c);
+	if (!irq_err || !irq_ev)
+	{
+		return;
+	}
+	ST_NVIC_configure_interrupt(irq_ev, priority, enable);
+	ST_NVIC_configure_interrupt(irq_err, priority, enable);
 }
 
 uint8_t ST_I2C_Master_send_IT(ST_I2C_t *i2c, const uint8_t slave_addr, const uint8_t *tx_buffer, const size_t data_len)
@@ -152,9 +198,9 @@ uint8_t ST_I2C_Master_send_IT(ST_I2C_t *i2c, const uint8_t slave_addr, const uin
 		i2c->irq.tx_buff_len = data_len;
 
 		// enable event, error and buffer interrupts
-		i2c->baseAdress->CR2 |= (1 << 10);
-		i2c->baseAdress->CR2 |= (1 << 9);
-		i2c->baseAdress->CR2 |= (1 << 8);
+		i2c->baseAdress->CR2 |= (1 << ST_I2C_CR2_ITERREN);
+		i2c->baseAdress->CR2 |= (1 << ST_I2C_CR2_ITEVEN);
+		i2c->baseAdress->CR2 |= (1 << ST_I2C_CR2_ITBUFEN);
 
 		// generate start condition
 		i2c->baseAdress->CR1 |= (1 << 8);
@@ -176,9 +222,9 @@ uint8_t ST_I2C_Master_receive_IT(ST_I2C_t *i2c, const uint8_t slave_addr, uint8_
 		i2c->irq.rx_size = data_len;
 
 		// enable event, error and buffer interrupts
-		i2c->baseAdress->CR2 |= (1 << 10);
-		i2c->baseAdress->CR2 |= (1 << 9);
-		i2c->baseAdress->CR2 |= (1 << 8);
+		i2c->baseAdress->CR2 |= (1 << ST_I2C_CR2_ITERREN);
+		i2c->baseAdress->CR2 |= (1 << ST_I2C_CR2_ITEVEN);
+		i2c->baseAdress->CR2 |= (1 << ST_I2C_CR2_ITBUFEN);
 
 		// generate start condition
 		i2c->baseAdress->CR1 |= (1 << 8);
@@ -188,4 +234,138 @@ uint8_t ST_I2C_Master_receive_IT(ST_I2C_t *i2c, const uint8_t slave_addr, uint8_
 	return busy;
 }
 
+void ST_I2C_irq_ev_handler(ST_I2C_t *i2c)
+{
+	const uint8_t i2c_ev_irq_enanbled = i2c->baseAdress->CR2 & (1 << ST_I2C_CR2_ITEVEN);
+	const uint8_t i2c_ev_buf_enabled = i2c->baseAdress->CR2 & (1 << ST_I2C_CR2_ITBUFEN);
 
+	if (i2c_ev_irq_enanbled)
+	{
+		const uint8_t sb_ev = i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_SB);
+		const uint8_t addr_ev = i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_ADDR);
+		const uint8_t btf_ev = i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_BTF);
+		const uint8_t stop_ev = i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_STOPF);
+
+		if (sb_ev)
+		{
+			// start bit sent, this means we can continue with the "address phase"
+			if (i2c->irq.irq_state == ST_I2C_IRQ_STATE_BUSY_RX)
+			{
+				// address phase for receiver (TODO)
+			}
+			else if (i2c->irq.irq_state == ST_I2C_IRQ_STATE_BUSY_TX)
+			{
+				// address phase to transmitter
+				// set RW bit to "W = 0" and send the slave address
+				uint8_t slave_addr_shifted = (i2c->irq.dev_addr << 1);
+				slave_addr_shifted &= ~(1);
+				i2c->baseAdress->DR = slave_addr_shifted;
+			}
+		}
+
+		if (addr_ev)
+		{
+			// address ACKed, clear ADDR flag by reading SR1 and SR2
+			uint32_t unused = i2c->baseAdress->SR1;
+			unused = i2c->baseAdress->SR2;
+			(void)unused;
+		}
+
+		if (btf_ev)
+		{
+			// data byte transfer finished
+			if (i2c->irq.irq_state == ST_I2C_IRQ_STATE_BUSY_RX)
+			{
+				;//nothing to do
+			}
+			else if (i2c->irq.irq_state == ST_I2C_IRQ_STATE_BUSY_TX)
+			{
+				// check if TXE is set and everything is transferred, then close the communication
+				if ((i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_TXE)) && (i2c->irq.tx_buff_len == 0))
+				{
+					// generate stop condition and reset the irq state
+					// only if not "repeated start"
+					if (!i2c->irq.rep_start)
+					{
+						i2c->baseAdress->CR1 |= (1 << 9);
+					}
+
+					// disable interrupts
+					i2c->baseAdress->CR2 &= ~(1 << ST_I2C_CR2_ITEVEN);
+					i2c->baseAdress->CR2 &= ~(1 << ST_I2C_CR2_ITBUFEN);
+
+					memset(&i2c->irq, 0, sizeof(i2c->irq));
+					i2c->irq.irq_state = ST_I2C_IRQ_STATE_IDLE;
+
+					ST_I2C_App_Event(i2c, ST_I2C_EVENT_TX_COMPL);
+				}
+			}
+		}
+
+		if (stop_ev)
+		{
+			// stop event received
+			// clear the stop flag (by reading SR1 and updating CR1)
+			// SR1 is read above, only write to CR1:
+			i2c->baseAdress->CR1 |= 0x0;
+
+			ST_I2C_App_Event(i2c, ST_I2C_EVENT_STOP);
+		}
+
+		if (i2c_ev_buf_enabled)
+		{
+			const uint8_t tx_ev = i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_TXE);
+			const uint8_t rx_ev = i2c->baseAdress->SR1 & (1 << ST_I2C_SR1_RXNE);
+
+			if (tx_ev && ST_I2C_is_master(i2c))
+			{
+				// transmit buffer empty, transfer the next byte
+				if ((i2c->irq.tx_buff_len > 0) && (i2c->irq.irq_state == ST_I2C_IRQ_STATE_BUSY_TX))
+				{
+					i2c->baseAdress->DR = *i2c->irq.tx_buffer;
+					++i2c->irq.tx_buffer;
+					--i2c->irq.tx_buff_len;
+				}
+			}
+
+			if (rx_ev)
+			{
+				// receive buffer not empty
+				if ((i2c->irq.rx_buff_len > 0) && (i2c->irq.irq_state == ST_I2C_IRQ_STATE_BUSY_RX))
+				{
+					//TODO master receiver
+				}
+			}
+		}
+	}
+}
+
+static void ST_I2C_irq_err_check_clear_notify(ST_I2C_t *i2c, uint8_t flag, uint8_t signal_code)
+{
+	const uint8_t err_flag = i2c->baseAdress->SR1 & (1 << flag);
+	if (err_flag)
+	{
+		i2c->baseAdress->SR1 &= ~(1 << flag);
+		ST_I2C_App_Event(i2c, signal_code);
+	}
+}
+
+void ST_I2C_irq_err_handler(ST_I2C_t *i2c)
+{
+	const uint8_t i2c_err_irq_enanbled = i2c->baseAdress->CR2 & (1 << ST_I2C_CR2_ITERREN);
+	if (i2c_err_irq_enanbled)
+	{
+		ST_I2C_irq_err_check_clear_notify(i2c, ST_I2C_SR1_BERR, ST_I2C_EVENT_ERR_BUS);
+		ST_I2C_irq_err_check_clear_notify(i2c, ST_I2C_SR1_ARLO, ST_I2C_EVENT_ERR_ARBITRATION_LOSS);
+		ST_I2C_irq_err_check_clear_notify(i2c, ST_I2C_SR1_AF, ST_I2C_EVENT_ERR_ACK_FAIL);
+		ST_I2C_irq_err_check_clear_notify(i2c, ST_I2C_SR1_OVR, ST_I2C_EVENT_ERR_OVERRUN);
+		ST_I2C_irq_err_check_clear_notify(i2c, ST_I2C_SR1_PEC, ST_I2C_EVENT_ERR_PEC);
+		ST_I2C_irq_err_check_clear_notify(i2c, ST_I2C_SR1_TMOUT, ST_I2C_EVENT_ERR_TIMEOUT);
+	}
+}
+
+void ST_I2C_App_Event(ST_I2C_t *pSpi, uint8_t e_type)
+{
+	(void)pSpi;
+	(void)e_type;
+}
